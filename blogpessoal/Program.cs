acd86d24787520afddeaa5357e6dec6cfea9ec79
Victor Paliari <victorrpaliari@gmail.com>
2023-10-04 10:12:46 -0300

    refatoração program.cs

--- blogpessoal/Program.cs
+++ blogpessoal/Program.cs
@@ -25,6 +25,7 @@ namespace blogpessoal
                 .AddNewtonsoftJson(options =>
                 {
                     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
                 });
 
             // Conexão com o banco de dados
